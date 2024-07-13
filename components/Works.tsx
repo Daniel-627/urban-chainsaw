c8d0a329e0fd821130fe23613875d4c8a109b371
Daniel-627 <ochiengdaniel627@gmail.com>
2024-07-14 01:08:35 +0300

    Eighth Commit

--- components/Works.tsx
+++ components/Works.tsx
@@ -9,11 +9,21 @@ const Projects = () => {
         <div
           key={project.id}
           className="relative border rounded-lg overflow-hidden shadow-lg h-96"
-          style={{ backgroundImage: `url(${project.img})`, backgroundSize: 'cover', backgroundPosition: 'center' }}
         >
-          <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col justify-end p-4">
+          <img src={project.img} alt={project.title} className="absolute inset-0 w-full h-full object-cover z-0" />
+          <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col justify-end p-4 z-10">
             <h3 className="text-xl font-bold text-white mb-2">{project.title}</h3>
             <p className="text-gray-300 mb-4">{project.des}</p>
+            <div className="flex space-x-2 mb-4">
+              {project.categories.map((category, index) => (
+                <span
+                  key={index}
+                  className="bg-blue-600 text-white text-xs font-semibold px-2 py-1 rounded"
+                >
+                  {category}
+                </span>
+              ))}
+            </div>
             <a
               href={project.link}
               className="text-blue-400 hover:underline"
@@ -30,3 +40,4 @@ const Projects = () => {
 };
 
 export default Projects;
+
