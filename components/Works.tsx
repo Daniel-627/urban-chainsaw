98612cb43dd0cd5372ab7eae76635e27dfbf0054
Daniel-627 <ochiengdaniel627@gmail.com>
2024-07-14 00:36:06 +0300

    Sixth Commit

--- components/Works.tsx
+++ components/Works.tsx
@@ -1,11 +1,24 @@
-import React from 'react'
+// app/Projects.tsx
+import React from 'react';
+import { projects } from '@/data';
 
-type Props = {}
-
-const Works = (props: Props) => {
+const Projects = () => {
   return (
-    <div>Works</div>
-  )
-}
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      {projects.map(project => (
+        <div key={project.id} className="border rounded-lg overflow-hidden shadow-lg p-4">
+          <img src={project.img} alt={project.title} className="w-full h-48 object-cover" />
+          <div className="p-4">
+            <h3 className="text-xl font-bold mb-2">{project.title}</h3>
+            <p className="text-gray-700 mb-4">{project.des}</p>
+            <a href={project.link} className="text-blue-500 hover:underline" target="_blank" rel="noopener noreferrer">
+              Visit Project
+            </a>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
 
-export default Works
+export default Projects;
