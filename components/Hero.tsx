dedb294de46f3abc8c84fff1e40f1e4d13dc610c
Daniel-627 <ochiengdaniel627@gmail.com>
2024-08-09 22:32:03 +0300

    19th commit

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Image from "next/image";
 import img1 from'@/public/img1.jpg'
+import { Animate } from './Animate';
 
 type Props = {}
 
@@ -19,7 +20,9 @@ const Hero = (props: Props) => {
         /> 
       </div>
       <div className='flex flex-col justify-items-center mx-56 text-center space-y-5 py-5'>
-        <p className='text-base font-extralight text-[#b0b0b0]'>Hi, I'm Daniel Ochieng</p>
+        {/*<Animate>
+          <p className='text-base font-extralight text-[#b0b0b0]'>Hi, I'm Daniel Ochieng</p>
+        </Animate>*/}
         <h1 className='text-4xl font-semibold text-[#ffffff]'>Building Bridges between Users and Technology through Design and Development.</h1>
         <p className='test-base font-light text-[#b0b0b0]'>a web designer and developer based in Kenya.<br/>I specialize in Ui/Ux Design, Responsive Web Design and Web Development.</p>
       </div>
