5be9551196387ec593f3786d32c2f9383fe3b487
Daniel-627 <ochiengdaniel627@gmail.com>
2024-09-02 23:38:34 +0300

    45th commit

--- data/index.ts
+++ data/index.ts
@@ -1,4 +1,4 @@
-import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin, FaGithub, FaYoutube, FaPinterest, FaSnapchat, FaTiktok } from 'react-icons/fa';
+import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin, FaGithub, FaYoutube, FaPinterest, FaSnapchat, FaTiktok, FaDribbble, FaBehance } from 'react-icons/fa';
 
 export const projects = [
   {
@@ -36,15 +36,12 @@ export const projects = [
 ];
 
 export const socialMediaLinks = [
-  { icon: FaFacebook, link: 'https://www.facebook.com' },
   { icon: FaTwitter, link: 'https://www.twitter.com' },
   { icon: FaInstagram, link: 'https://www.instagram.com' },
   { icon: FaLinkedin, link: 'https://www.linkedin.com' },
   { icon: FaGithub, link: 'https://www.github.com' },
-  { icon: FaYoutube, link: 'https://www.youtube.com' },
-  { icon: FaPinterest, link: 'https://www.pinterest.com' },
-  { icon: FaSnapchat, link: 'https://www.snapchat.com' },
-  { icon: FaTiktok, link: 'https://www.tiktok.com' },
+  { icon: FaDribbble, link: 'https://www.github.com' },
+  { icon: FaBehance, link: 'https://www.github.com' },
 ];
 
 
